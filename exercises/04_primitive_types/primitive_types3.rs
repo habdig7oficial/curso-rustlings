276ce5387e05cd570aa86a10f43c0076a9d1e67c
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2024-12-08 20:50:08 -0300

    rustlings no tablet kkkk

--- exercises/04_primitive_types/primitive_types3.rs
+++ exercises/04_primitive_types/primitive_types3.rs
@@ -1,7 +1,13 @@
 fn main() {
     // TODO: Create an array called `a` with at least 100 elements in it.
     // let a = ???
-
+    
+    let a: [i32; 100] = [42; 100];
+  
+   for i in 0..a.len(){
+     println!("{i}) {}", a[i])
+   }
+  
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
     } else {
