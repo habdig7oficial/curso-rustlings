a87cf2aa3a97c73374a27b32a07b79c4098798d5
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2024-12-09 02:07:19 -0300

    rustlings no tablet kkkk

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -1,13 +1,32 @@
+#[allow(unused_parens)]
 fn trim_me(input: &str) -> &str {
     // TODO: Remove whitespace from both ends of a string.
+    let mut ptr: *const &str = &input;
+    for i in input.chars().rev(){
+       if(i == ' '){
+         println!("{:?}", ptr);
+        // ptr = ptr.wrapping_add(1);
+        ptr = ptr.wrapping_add(1);
+        let a: &str = *ptr;
+        println!("{}",a);
+       }
+       else {
+         break;
+       }
+       println!("{i}) ");
+    }
+    return input;
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There are multiple ways to do this.
+   return String::from("");
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons".
+    
+    return String::from("");
 }
 
 fn main() {
